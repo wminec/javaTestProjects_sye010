95caddb447e74de3116857a064070a3d1f479052
wminec <wminec@gmail.com>
2025-03-12 22:54:47 +0900

    Update TestController.java

--- printAllResponseHeader-jdk8-springboot-maven/src/main/java/com/printhttpheaders/test/controller/TestController.java
+++ printAllResponseHeader-jdk8-springboot-maven/src/main/java/com/printhttpheaders/test/controller/TestController.java
@@ -11,11 +11,11 @@ public class TestController {
 
     @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
     public String index(HttpServletRequest httpRequest) {
-        Enumeration<String> enumeration = httpRequest.getHeaderNames(); 
+        Enumeration<String> enumeration = httpRequest.getHeaderNames();
         String ret = "";
 
-        while (enumeration.hasMoreElements()) { 
-            String name = enumeration.nextElement(); 
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
             String value = httpRequest.getHeader(name);
             ret += name + " : " + value + "<p>";
         }
@@ -23,24 +23,24 @@ public class TestController {
         String[] headerTypes = {"X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
                                 "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR"};
         String ip = null;
-        
+        String header = null;
+
         for (String headerType : headerTypes) {
             ip = httpRequest.getHeader(headerType);
             if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+                header = headerType;
+                ret += "remoteIP(" + header + ") : " + ip + "<p>";
                 break;
             }
         }
 
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = httpRequest.getRemoteAddr();
-        }
+        ret += "remoteIP(getRemoteAddr) : " + httpRequest.getRemoteAddr();
 
         // 여러 개의 IP가 있을 경우, 첫 번째 IP만 가져오기
         //if (ip != null && ip.contains(",")) {
         //    ip = ip.split(",")[0].trim();
         //}
 
-        ret += "remoteIP : " + ip;
 
         return ret;
     }
